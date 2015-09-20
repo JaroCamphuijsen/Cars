1ca67c1787829c09ba27b89de775f4c74348acaa
eavw <eva.v.weel@live.nl>
2015-09-20 13:51:15 +0200

    Comments en outline toegevoegd aan de data (stond nog in de opdracht)

--- Assignment 2/VolumeReader.py
+++ Assignment 2/VolumeReader.py
@@ -2,146 +2,93 @@
 """
 Created on Wed Sep 16 14:27:37 2015
 
-@author: Eva van Weel
+@author: Jaromir Camphuijsen (6042473) and Eva van Weel(10244743)
 """
 
-#!/usr/bin/env python
-
-# This example reads a volume dataset, extracts an isosurface that
-# represents the skin and displays it.
-
-import sys
 import Tkinter
 import vtk
 from vtk.tk.vtkTkRenderWidget import vtkTkRenderWidget
 
-
-
-# Create the renderer, the render window, and the interactor. The
-# renderer draws into the render window, the interactor enables mouse-
-# and keyboard-based interaction with the scene.
+# Create the renderer and the GUI
+root = Tkinter.Tk() 
 aRenderer = vtk.vtkRenderer()
 aRenderer.TexturedBackgroundOn()
-renWin = vtk.vtkRenderWindow()
+renderWidget = vtkTkRenderWidget(root,width=800,height=600)
+renderWidget.pack(expand='true',fill='both')
+renWin = renderWidget.GetRenderWindow()
 renWin.AddRenderer(aRenderer)
-iren = vtk.vtkRenderWindowInteractor()
-style = vtk.vtkInteractorStyleTrackballCamera()
-iren.SetInteractorStyle(style)
-iren.SetRenderWindow(renWin)
-# The following reader is used to read a series of 2D slices (images)
-# that compose the volume. The slice dimensions are set, and the
-# pixel spacing. The data Endianness must also be specified. The reader
-# usese the FilePrefix in combination with the slice number to construct
-# filenames using the format FilePrefix.%d. (In this case the FilePrefix
-# is the root name of the file: quarter.)
-v16 = vtk.vtkVolume16Reader()
-v16.SetDataDimensions(256, 256)
-v16.SetDataByteOrderToLittleEndian()
-v16.SetFilePrefix(".\Data\slice")
-v16.SetImageRange(1, 94)
-
-
-# spacing = v16.GetOutput().GetSpacing()
-spacing = lambda: [3.2, 3.2, 5]
-print spacing()
-sx, sy, sz = spacing()
-v16.SetDataSpacing(sx, sy, sz)
-
-v16.Update()
-scalarMin, scalarMax = v16.GetOutput().GetScalarRange()
-#print sz
-
-# im = v16.GetImage(6)
-
-# histogram = vtk.vtkImageAccumulate()
-# histogram.SetInputConnection(im.GetOutputPort())
-# histogram.SetComponentExtent(0,255,0,0,0,0)
-# histogram.SetComponentOrigin(0,0,0)
-# histogram.SetComponentSpacing(1,0,0)
-# histogram.IgnoreZeroOn()
-# histogram.Update()
-
-# An isosurface, or contour value of 500 is known to correspond to the
-# skin of the patient. Once generated, a vtkPolyDataNormals filter is
-# is used to create normals for smooth surface shading during rendering.
-# The triangle stripper is used to create triangle strips from the
-# isosurface these render much faster on may systems.
-skinExtractor = vtk.vtkContourFilter()
-skinExtractor.SetInputConnection(v16.GetOutputPort())
-
-skinNormals = vtk.vtkPolyDataNormals()
-skinNormals.SetInputConnection(skinExtractor.GetOutputPort())
-#skinNormals.SetFeatureAngle(20.0)
-skinMapper = vtk.vtkPolyDataMapper()
-skinMapper.SetInputConnection(skinNormals.GetOutputPort())
-skinMapper.ScalarVisibilityOff()
-skin = vtk.vtkActor()
-skin.SetPosition(400, 200, 400)
-skin.SetOrientation(270, 0, 180)
-skin.SetMapper(skinMapper)
-
-
-
-# It is convenient to create an initial view of the data. The FocalPoint
-# and Position form a vector direction. Later on (ResetCamera() method)
-# this vector is used to position the camera to look at the data in
-# this direction.
-aCamera = vtk.vtkCamera()
-aCamera.SetPosition(0,0, -4000)
-
-
-
-aCamera.ComputeViewPlaneNormal()
-
-# Actors are added to the renderer. An initial camera view is created.
-# The Dolly() method moves the camera towards the FocalPoint,
-# thereby enlarging the image.
-aRenderer.AddActor(skin)
-aRenderer.SetActiveCamera(aCamera)
-aRenderer.ResetCamera()
-aCamera.Dolly(1.5)
-
-# Set a background color for the renderer and set the size of the
-# render window (expressed in pixels).
 aRenderer.SetBackground(1, 1, 1)
 renWin.SetSize(600, 480)
 
+# Read the 2D images (CT scan slices). 
+v16 = vtk.vtkVolume16Reader()
+v16.SetDataDimensions(256, 256)
+v16.SetDataByteOrderToLittleEndian()
+v16.SetFilePrefix(".\Data\slice") #Loop through all slices
+v16.SetImageRange(1, 94) #Number of images to loop through
+
+#Slice spacing
+spacing = lambda: [3.2, 3.2, 5]
+sx, sy, sz = spacing()
+v16.SetDataSpacing(sx, sy, sz)
+v16.Update()
+#Read the scalar value range
+scalarMin, scalarMax = v16.GetOutput().GetScalarRange()
+
+
+#Create an isosurface and use vtkPolyDataNormals to create normals for smooth 
+#surface shading.The triangle stripper is used to create triangle strips from 
+#the isosurface which render much faster on many systems.
+skinExtractor = vtk.vtkContourFilter()
+skinExtractor.SetInputConnection(v16.GetOutputPort())
+skinNormals = vtk.vtkPolyDataNormals()
+skinNormals.SetInputConnection(skinExtractor.GetOutputPort())
+skinNormals.SetFeatureAngle(60.0)
+skinStripper = vtk.vtkStripper()
+skinStripper.SetInputConnection(skinNormals.GetOutputPort())
+skinMapper = vtk.vtkPolyDataMapper()
+skinMapper.SetInputConnection(skinStripper.GetOutputPort())
+skinMapper.ScalarVisibilityOff()
+skin = vtk.vtkActor()
+skin.SetMapper(skinMapper)
+
+#Create outline to show extent of the data.
+outlineData = vtk.vtkOutlineFilter()
+outlineData.SetInputConnection(v16.GetOutputPort())
+mapOutline = vtk.vtkPolyDataMapper()
+mapOutline.SetInputConnection(outlineData.GetOutputPort())
+outline = vtk.vtkActor()
+outline.SetMapper(mapOutline)
+outline.GetProperty().SetColor(0, 0, 0)
+
+#Add actors to the renderer
+aRenderer.AddActor(outline)
+aRenderer.AddActor(skin)
+
+# Camera (viewpoint) settings
+aCamera = vtk.vtkCamera()
+aCamera.SetPosition(0,0, -4000)
+aCamera.ComputeViewPlaneNormal()
+aRenderer.SetActiveCamera(aCamera)
+aRenderer.ResetCamera()
+aCamera.Dolly(1.5) #Move camera to vocal point
+
+#Class which allows the interactivity of the slider. Based on code from:
+#http://www.uppmax.uu.se/docs/w/index.php/TkInter
 class scale:
     "Scale"
-    def __init__(self, root, renWin, sphere):
-        self.renWin, self.sphere = renWin, sphere
-        scale = Tkinter.Scale(root, from_=scalarMin, to=scalarMax,resolution=.1, orient= "horizontal", command=self.change)
+    def __init__(self, root, renWin, skinExtractor):
+        self.renWin, self.skinExtractor = renWin, skinExtractor
+        scale = Tkinter.Scale(root, length=1000, from_=scalarMin, to=scalarMax,resolution=.1, orient= "horizontal", command=self.change)
+        scale.set(scalarMax/2)  
         scale.pack(side='bottom')
 
     def change(self, val):
-        # This strange int(float()) conversion is required....
         skinExtractor.SetValue(0, int(float(val)))
         self.renWin.Render()
 
 
-root = Tkinter.Tk() 
-
-renderWidget = vtkTkRenderWidget(root,width=800,height=600)
-renderWidget.pack(expand='true',fill='both')
-
-renWin = renderWidget.GetRenderWindow()
-
-ren = vtk.vtkRenderer()
-renWin.AddRenderer(aRenderer)
-ren.AddActor(skin)
-
 scale=scale(root, renWin, skinExtractor)
-
-
-
-
-
-
-
-# Interact with the data.
-#iren.Initialize()
-#renWin.Render()
-#iren.Start()
 root.mainloop()
 
 
