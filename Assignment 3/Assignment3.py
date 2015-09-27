91f3ebf1327911db2fc756ca950c4a744e2d2486
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2015-09-27 16:42:42 +0200

    Multiple particle sources and adjusted mapping

--- Assignment 3/Assignment3.py
+++ Assignment 3/Assignment3.py
@@ -7,7 +7,8 @@ Created on Wed Sep 16 14:27:37 2015
 @author: Jaromir Camphuijsen (6042473) and Eva van Weel(10244743)
 """
 
-#This code is based on code found at: http://www.vtk.org/gitweb?p=VTK.git;a=blob;f=Examples/VisualizationAlgorithms/Python/officeTubes.py
+#This code is based on code found at: http://www.vtk.org/gitweb?p=VTK.git;
+#a=blob;f=Examples/Medical/Python/Medical3.py
 
 import Tkinter
 import vtk
@@ -28,6 +29,9 @@ renWin.SetSize(600, 480)
 reader = vtk.vtkStructuredPointsReader()
 reader.SetFileName("./SMRX.vtk")
 
+scalarMin, scalarMax = reader.GetOutput().GetScalarRange()
+
+
 #Create an isosurface and use vtkPolyDataNormals to create normals for smooth 
 #surface shading.
 contourFilter = vtk.vtkContourFilter()
@@ -40,16 +44,55 @@ dataMapper = vtk.vtkPolyDataMapper()
 dataMapper.SetInputConnection(contourNormals.GetOutputPort())
 dataMapper.ScalarVisibilityOff() 
 contour = vtk.vtkActor()
-#contour.SetPosition(400, 200, 400)
-#contour.SetOrientation(270, 0, 180)
+# contour.SetPosition(400, 200, 400)
+# contour.SetOrientation(270, 0, 180)
 contour.SetMapper(dataMapper)
+contour.GetProperty().SetOpacity(0.25)
+
+
+length = reader.GetOutput().GetLength()
+
+# maxVelocity =reader.GetOutput().GetPointData().GetVectors().GetMaxNorm()
+# maxTime = 35.0*length/maxVelocity
+
+# Now we will generate a single streamline in the data. We select the
+# integration order to use (RungeKutta order 4) and associate it with
+# the streamer. The start position is the position in world space
+# where we want to begin streamline integration; and we integrate in
+# both directions. The step length is the length of the line segments
+# that make up the streamline (i.e., related to display). The
+# IntegrationStepLength specifies the integration step length as a
+# fraction of the cell size that the streamline is in.
+
+
+# integ = vtk.vtkRungeKutta4()
+# streamer = vtk.vtkStreamLine()
+# streamer.SetInputConnection(reader.GetOutputPort())
+# streamer.SetIntegrator(integ)
+# streamer.SetMaximumPropagationTime(500)
+# streamer.SetStepLength(0.5)
+# streamer.SetIntegrationStepLength(0.05)
+# streamer.SetIntegrationDirectionToIntegrateBothDirections()
+
+# seeds = vtk.vtkPointSource()
+# seeds.SetRadius(0.15)
+# seeds.SetCenter(60,30,30)
+# seeds.SetNumberOfPoints(100)
+# # print dir(streamer)
+# streamer.SetSource(seeds.GetOutputPort())
 
 # Create source for streamtubes
-seeds = vtk.vtkPointSource()
-seeds.SetRadius(31) #half of data extent
-seeds.SetCenter(60.5,30.5,30.5) #Center of data outline
-seeds.SetNumberOfPoints(100)
+seeds1 = vtk.vtkPointSource()
+seeds1.SetRadius(15) #half of data extent
+seeds1.SetCenter(0,15.5,30.5) #Center of data outline
+seeds1.SetNumberOfPoints(200)
 
+seeds2 = vtk.vtkPointSource()
+seeds2.SetRadius(15) #half of data extent
+seeds2.SetCenter(0,45.5,30.5) #Center of data outline
+seeds2.SetNumberOfPoints(200)
+
+print seeds1
 #length = reader.GetOutput().GetLength()
 
 #maxVelocity =reader.GetOutput().GetPointData().GetVectors().GetMaxNorm()
@@ -65,39 +108,73 @@ seeds.SetNumberOfPoints(100)
 # # IntegrationStepLength specifies the integration step length as a
 # # fraction of the cell size that the streamline is in.
 integ = vtk.vtkRungeKutta45()
-streamer = vtk.vtkStreamTracer()
-streamer.SetInputConnection(reader.GetOutputPort())
-streamer.SetSourceConnection(seeds.GetOutputPort())
-streamer.SetMaximumPropagation(500) 
-streamer.SetIntegrationStepUnit(2) #Cell length unit
-streamer.SetMinimumIntegrationStep(0.01)
-streamer.SetMaximumIntegrationStep(0.5)
-streamer.SetInitialIntegrationStep(0.2)
-streamer.SetIntegrationDirection(2)
-streamer.SetIntegrator(integ)
-streamer.SetRotationScale(0.5)
-streamer.SetMaximumError(1.0e-8)
- 
- # The tube is wrapped around the generated streamline. By varying the
- # radius by the inverse of vector magnitude, we are creating a tube
- # whose radius is proportional to mass flux (in incompressible flow).
-streamTube = vtk.vtkTubeFilter()
-streamTube.SetInputConnection(streamer.GetOutputPort())
-streamTube.SetRadius(0.5)
-streamTube.SetNumberOfSides(12)
-streamTube.SetVaryRadiusToVaryRadiusByVector()
- 
-mapStreamTube = vtk.vtkPolyDataMapper()
-mapStreamTube.SetInputConnection(streamTube.GetOutputPort())
-mapStreamTube.SetScalarRange(reader.GetOutput().GetScalarRange())
- 
-streamTubeActor = vtk.vtkActor()
-streamTubeActor.SetMapper(mapStreamTube)
-#streamTubeActor.GetProperty().BackfaceCullingOn()
+streamer1 = vtk.vtkStreamTracer()
+streamer1.SetInputConnection(reader.GetOutputPort())
+streamer1.SetSourceConnection(seeds1.GetOutputPort())
+streamer1.SetMaximumPropagation(2000) 
+streamer1.SetIntegrationStepUnit(2) #Cell length unit
+streamer1.SetMinimumIntegrationStep(0.01)
+streamer1.SetMaximumIntegrationStep(10)
+streamer1.SetInitialIntegrationStep(0.2)
+streamer1.SetIntegrationDirection(0)
+streamer1.SetIntegrator(integ)
+streamer1.SetRotationScale(0.5)
+# streamer1.SetMaximumError(1.0e-8)
+
+streamer2 = vtk.vtkStreamTracer()
+streamer2.SetInputConnection(reader.GetOutputPort())
+streamer2.SetSourceConnection(seeds2.GetOutputPort())
+streamer2.SetMaximumPropagation(2000) 
+streamer2.SetIntegrationStepUnit(2) #Cell length unit
+streamer2.SetMinimumIntegrationStep(0.01)
+streamer2.SetMaximumIntegrationStep(10)
+streamer2.SetInitialIntegrationStep(0.2)
+streamer2.SetIntegrationDirection(0)
+streamer2.SetIntegrator(integ)
+streamer2.SetRotationScale(0.5)
+# streamer2.SetMaximumError(1.0e-8)
+
+# The tube is wrapped around the generated streamline. By varying the
+# radius by the inverse of vector magnitude, we are creating a tube
+# whose radius is proportional to mass flux (in incompressible flow).
+streamTube1 = vtk.vtkTubeFilter()
+streamTube1.SetInputConnection(streamer1.GetOutputPort())
+streamTube1.SetRadius(.1)
+streamTube1.SetNumberOfSides(12)
+
+
+streamTube2 = vtk.vtkTubeFilter()
+streamTube2.SetInputConnection(streamer2.GetOutputPort())
+streamTube2.SetRadius(.1)
+streamTube2.SetNumberOfSides(12)
+
+vLookupTable = vtk.vtkLookupTable()
+vLookupTable.SetValueRange(0.5,1)
+
+mapStreamTube1 = vtk.vtkPolyDataMapper()
+mapStreamTube1.SetInputConnection(streamTube1.GetOutputPort())
+mapStreamTube1.SetLookupTable(vLookupTable)
+mapStreamTube1.SetScalarVisibility(0)	
 
 
 
+mapStreamTube2 = vtk.vtkPolyDataMapper()
+mapStreamTube2.SetInputConnection(streamTube2.GetOutputPort())
+mapStreamTube2.SetLookupTable(vLookupTable)
+mapStreamTube2.SetScalarVisibility(0)
 
+
+
+streamTubeActor1 = vtk.vtkActor()
+streamTubeActor1.SetMapper(mapStreamTube1)
+# streamTubeActor1.GetProperty().BackfaceCullingOn()
+streamTubeActor1.GetProperty().SetColor(1, 0, 0)
+
+
+streamTubeActor2 = vtk.vtkActor()
+streamTubeActor2.SetMapper(mapStreamTube2)
+# streamTubeActor2.GetProperty().BackfaceCullingOn()
+streamTubeActor2.GetProperty().SetColor(0,1,0)
 
 #Create outline to show extent of the data.
 outlineData = vtk.vtkOutlineFilter()
@@ -105,15 +182,16 @@ outlineData.SetInputConnection(reader.GetOutputPort())
 mapOutline = vtk.vtkPolyDataMapper()
 mapOutline.SetInputConnection(outlineData.GetOutputPort())
 outline = vtk.vtkActor()
-#outline.SetPosition(400, 200, 400)
-#outline.SetOrientation(270, 0, 180)
+# outline.SetPosition(400, 200, 400)
+# outline.SetOrientation(270, 0, 180)
 outline.SetMapper(mapOutline)
 outline.GetProperty().SetColor(0, 0, 0)
 
 #Add actors to the renderer
 aRenderer.AddActor(outline)
+aRenderer.AddActor(streamTubeActor1)
+aRenderer.AddActor(streamTubeActor2)
 aRenderer.AddActor(contour)
-aRenderer.AddActor(streamTubeActor)
 
 # Camera (viewpoint) settings
 aCamera = vtk.vtkCamera()
